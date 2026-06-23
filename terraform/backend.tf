28c15904e201ad4f96f20a00a39cec1c7bf67085
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-23 13:23:58 +0200

    fix: Fixed some envvars

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,11 +5,11 @@ terraform {
     region = "fr-par"
 
     # Scaleway Object Storage is S3-compatible, not AWS.
-    endpoints                   = { s3 = "https://s3.fr-par.scw.cloud" }
+    endpoints = {
+      s3 = "https://s3.fr-par.scw.cloud"
+    }
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    use_path_style              = false
   }
 }
