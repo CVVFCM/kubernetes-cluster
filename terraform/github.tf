917d5c3581f07ea7f432cbdea75c9f52ef255222
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-25 23:09:50 +0200

    feat: Added OCI vars

--- terraform/github.tf
+++ terraform/github.tf
@@ -6,3 +6,34 @@ resource "github_actions_organization_secret" "kubeconfig" {
   visibility  = "all"
   value       = data.oci_containerengine_cluster_kube_config.main.content
 }
+
+resource "github_actions_organization_secret" "oci_tenancy" {
+  secret_name = "OCI_TENANCY_OCID"
+  visibility  = "all"
+  value       = var.tenancy_id
+}
+
+resource "github_actions_organization_secret" "oci_user" {
+  secret_name = "OCI_USER_OCID"
+  visibility  = "all"
+  value       = var.user_id
+}
+
+resource "github_actions_organization_secret" "oci_fingerprint" {
+  secret_name = "OCI_FINGERPRINT"
+  visibility  = "all"
+  value       = var.fingerprint
+}
+
+resource "github_actions_organization_secret" "oci_region" {
+  secret_name = "OCI_REGION"
+  visibility  = "all"
+  value       = var.region
+}
+
+# Terraform lit ton fichier .pem local et l'envoie dans GitHub de manière sécurisée
+resource "github_actions_organization_secret" "oci_api_key" {
+  secret_name = "OCI_API_KEY"
+  visibility  = "private"
+  value       = file(var.private_key_path)
+}
